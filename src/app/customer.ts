37f64798bac21320be611751a696eb444b9d78f7
SingamNavya312 <86612637+SingamNavya312@users.noreply.github.com>
2022-06-06 14:33:02 +0530

    Add files via upload

---
+++ src/app/customer.ts
@@ -0,0 +1,15 @@
+export interface customer {
+
+    
+        orderId: number;
+        customerName: string;
+        customerMobileNumber: number;
+        location: string;
+        pizzaName: string;
+        orderAmount: number;
+        orderdDate: Date;
+    
+    
+    
+    
+}
